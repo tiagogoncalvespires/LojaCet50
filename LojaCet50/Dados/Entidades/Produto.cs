f83c42bd8d17ce17ccfd0cff6cb84b2aed9c9af0
Tiago Pires <Tiago.Goncalves.Pires@formandos.cinel.pt>
2020-12-17 23:01:27 +0000

    CRUD of products for web.

--- LojaCet50/Dados/Entidades/Produto.cs
+++ LojaCet50/Dados/Entidades/Produto.cs
@@ -11,7 +11,7 @@ namespace LojaCet50.Dados.Entidades
         public string Nome { get; set; }
 
 
-
+        [Display(Name = "Preço")]
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Preco { get; set; }
 
