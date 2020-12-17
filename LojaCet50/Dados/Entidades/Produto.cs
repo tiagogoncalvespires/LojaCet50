65024d7b25f7502633b7b39dad97c0736876c503
Tiago Pires <Tiago.Goncalves.Pires@formandos.cinel.pt>
2020-12-17 22:42:20 +0000

    Create a product database and table.

---
+++ LojaCet50/Dados/Entidades/Produto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaCet50.Dados.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+
+        public string Nome { get; set; }
+
+
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Preco { get; set; }
+
+
+        [Display(Name = "Image")]
+        public string UrlDaImagem { get; set; }
+
+
+        [Display(Name = "Última compra")]
+        public DateTime UltimaCompra { get; set; }
+
+
+        [Display(Name = "Última venda")]
+        public DateTime UltimaVenda { get; set; }
+
+
+        [Display(Name = "Disponível")]
+        public bool Disponivel { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+    }
+}
